1503efeb1d816133e2a8e8ca0fd80a984b4cf830
Jan Sipos <jansipos@yahoo.com>
2015-06-07 15:46:15 +0200

    Multiple lemma output

--- src/com/jansipos/lemmatizer/Lemmatizer.java
+++ src/com/jansipos/lemmatizer/Lemmatizer.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -24,11 +23,10 @@ public final class Lemmatizer {
 	private static Map<String, Integer> results;
 
 	private static List<String> lines;
-	private static StringBuilder sb;
 	private static int unknown;
 
 	public static Lemmatizer instance;
-	
+
 	private static final String[] LIST_QUE_WORDS = new String[] {"absque", "abusque", "adaeque", "adque", "adusque", "aeque", "alterutraque", "alterutrique", "antique", "apsque", "atque", "circumquaque", "circumundique", "conseque", "coque", "cuiusmodicumque", "cumque", "cuncumque", "denique", "deque", "hocusque", "hucusque", "itaque", "iamiamque", "longinque", "namque", "neque", "peraeque", "plerumque", "quacumque", "quacunque", "qualitercumque", "qualitercunque", "quandiucumque", "quandocumque", "quandocunque", "quandoque", "quantumcumque", "quinque", "quoadusque", "quocumque", "quocunque", "quomodocumque", "quomodocunque", "quoque", "quoquomque", "quotcumque", "quotcunque", "quotienscumque", "quotienscunque", "quotiensquonque", "quotiescumque", "quotiescunque", "quotiesquonque", "quotusquisque", "quousque", "sesque", "simulatque", "susque", "ubicumque", "ubiquaque", "ubique", "ubiquomque", "undecumque", "undique", "usque", "usquequaque", "utcumque", "utcunque", "utique", "utquomque", "utrimque", "utrinque", "utrobique", "utroque", "utrubique"};
 	private static final Set<String> SET_QUE_WORDS = new HashSet<String>(Arrays.asList(LIST_QUE_WORDS));
 
@@ -45,10 +43,10 @@ public final class Lemmatizer {
 	}
 
 	public void lemmatize(File input) {
-		
+
 		results.clear();
 		unknown = 0; // resetira broj nepoznatih oblika
-		
+
 		dirOut = input.getParent() + "/out/lemmata-" + input.getName();
 
 		try {
@@ -56,8 +54,6 @@ public final class Lemmatizer {
 		} catch (IOException e1) {
 			System.out.println("Can't read input file: " + e1.getMessage());
 		}
-
-		sb = new StringBuilder();
 
 		for (String line : lines) {
 			String[] words = line.split(" ");
@@ -70,15 +66,14 @@ public final class Lemmatizer {
 					}
 
 					Entry entry = dictionary.get(word);
-					String lemma = entry.getLemma();
-					
+					String lemma = entry.getLemma().toString();
+
 					if (results.containsKey(lemma)) { // ako je ta lema već viđena
 						results.put(lemma, results.get(lemma) + 1); // dodaj 1 na broj pojavaka
 					}
 					else {						
-						results.put(entry.getLemma(), 1);
+						results.put(lemma, 1);
 					}
-//					sb.append(entry.getLemma() + "\n");
 				} catch (NullPointerException e) {
 					// System.out.println(word);
 					unknown++;
@@ -90,40 +85,43 @@ public final class Lemmatizer {
 	}
 
 	private void printResults() {
-		
+
 		StringBuilder sb = new StringBuilder();
+
+		// HashMap s rezultatima sortira se tako da se sve vrijednosti prebace u TreeMap (zadržava poredak)
+		// kako bi TreeMap znao kako treba sortirati, definira se comparator
 		
 		ValueComparator comparator =  new ValueComparator(results);
 		TreeMap<String, Integer> sorted = new TreeMap<String, Integer>(comparator);
-		
+
 		sorted.putAll(results);
-		
+
 		for (Map.Entry<String, Integer> entry : sorted.entrySet()) {
 			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
 		}
-		
+
 		try {
 			FileUtils.write(new File(dirOut), sb.toString());
 		} catch (IOException e) {
 			System.out.println("Can't write to output file: " + e.getMessage());
 		}
 	}
-	
+
 	private static class ValueComparator implements Comparator<String> {
 
 		Map<String, Integer> base;
-	    
-		public ValueComparator(Map<String, Integer> base) {
-	        this.base = base;
-	    }
 
-	    public int compare(String a, String b) {
-	        if (base.get(a) >= base.get(b)) {
-	            return -1;
-	        } else {
-	            return 1;
-	        }
-	    }
+		public ValueComparator(Map<String, Integer> base) {
+			this.base = base;
+		}
+
+		public int compare(String a, String b) {
+			
+			if (base.get(a) >= base.get(b))
+				return -1; // sortira od najvećeg prema najmanjem
+			
+			return 1;
+		}
 	}
 
 	private static class POSLemmaList {
@@ -133,8 +131,6 @@ public final class Lemmatizer {
 		private HashMap<String, Entry> dictionary;
 		private static List<String> forms;
 		private static List<String> lines;
-		private static String lemma;
-		private static String POS;
 
 		private static POSLemmaList instance;
 
@@ -156,16 +152,37 @@ public final class Lemmatizer {
 
 			try {
 				lines = FileUtils.readLines(file);
-
-				for (String line : lines) {
+				
+				// za svaki red u tekstu
+				for (String line : lines) { 
+					
+					// podijeli red po razmacima
+					// koristi LinkedList jer kod uklanjanja elemenata ne kopira u manji array kao ArrayList 
+					
 					forms = new LinkedList<String>(Arrays.asList(line.split(" ")));
-					POS = forms.remove(0);
-					lemma = forms.get(0);
+					String POS = forms.remove(0); // prvi je oblik u retku vrsta riječi
+					
+					HashSet<String> lemma = new HashSet<String>(); // radi se set, jer oblik potencijalno može imati više lema
+					lemma.add(forms.get(0)); // nakon uklanjanja POS-a, prvi je oblik lema, dodaje se u prazan set
 
 					Entry entry = new Entry(lemma, POS);
-
+					
+					// za svaki oblik pripadajuće leme
 					for (String form : forms) {
-						dictionary.put(form, entry);
+						
+						// ako postoji homonimni oblik u rječniku
+						if (dictionary.containsKey(form)) {
+							
+							// dohvati set lema zabilježenih za taj oblik
+							HashSet<String> existingLemma = dictionary.get(form).getLemma();
+							
+							// ako trenutna lema nije homonimna nekoj iz seta postojećih lema 
+							if (! existingLemma.contains(lemma)) {
+								existingLemma.addAll(lemma); // dodaj u set postojećih lema
+								dictionary.get(form).setLemma(existingLemma); // pridruži ažurirani set lema ovom obliku
+							}
+						}
+						dictionary.put(form, entry); // spremi oblik i leme u rječnik
 					}
 				}
 			} catch (IOException e) {
@@ -181,16 +198,20 @@ public final class Lemmatizer {
 
 	private static class Entry {
 
-		private String lemma;
+		private HashSet<String> lemma;
 		private String POS;
 
-		public Entry(String lemma, String POS) {
+		public Entry(HashSet<String> lemma, String POS) {
 			this.lemma = lemma;
 			this.POS = POS;
 		}
 
-		public String getLemma() {
+		public HashSet<String> getLemma() {
 			return lemma;
+		}
+		
+		public void setLemma(HashSet<String> lemma) {
+			this.lemma = lemma;
 		}
 
 		public String getPOS() {
