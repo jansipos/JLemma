4a170b32f4128500d5d5210417c5ff773f66e840
Jan Sipos <jansipos@yahoo.com>
2015-06-07 23:40:17 +0200

    Added GUI

--- src/com/jansipos/lemmatizer/Main.java
+++ src/com/jansipos/lemmatizer/Main.java
@@ -1,31 +1,85 @@
 package com.jansipos.lemmatizer;
 
+import java.awt.Desktop;
 import java.io.File;
-import java.util.Iterator;
+import java.io.IOException;
+import java.util.List;
 
-import org.apache.commons.io.FileUtils;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+	private static String initialDir = "res/text/test";
+	private static Button button;
+	private static File chosenFolder;
 	
-	static String dir = "res/text/test";
-	static String[] extensions = new String[]{"txt"};
+	private static final int WIDTH = 500;
+	private static final int HEIGHT = 100;
+	
+	private static Lemmatizer lemmatizer;
 
 	public static void main(String[] args) throws Exception {
-		long start = System.currentTimeMillis();
-		
-		Iterator<File> iterator = FileUtils.iterateFiles(new File(dir), extensions, false);
-		Lemmatizer lemmatizer = Lemmatizer.getInstance();
- 
-		while (iterator.hasNext()) {
-			lemmatizer.lemmatize(iterator.next());
-		}
+		lemmatizer = Lemmatizer.getInstance();
+		launch(args);
+	}
 
+	public void start(Stage primaryStage) throws Exception {
+
+		button = new Button("Browse");
+		button.setOnAction(e -> {
+
+			FileChooser fc = new FileChooser();
+			fc.setTitle("Choose files");
+			fc.setInitialDirectory(new File(initialDir));
+
+			FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("txt", "*.txt");
+			fc.getExtensionFilters().add(extFilter);
+
+			List<File> chosenFiles = fc.showOpenMultipleDialog(primaryStage);
+
+			if (chosenFiles != null && !chosenFiles.isEmpty()) {
+				chosenFolder = chosenFiles.get(0).getParentFile();
+				
+				chosenFiles.forEach(f -> lemmatizer.lemmatize(f));
+				displayDialogAndFiles();
+			}
+		});
+
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+
+		Scene scene = new Scene(layout, WIDTH, HEIGHT);
+
+		primaryStage.setTitle("Lemmatizer");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	private void displayDialogAndFiles() {
 		
-		// debugging
-		long end = System.currentTimeMillis();
-		System.out.println(end - start);
+		Alert alert = new Alert(AlertType.INFORMATION);
 		
-//		FileUtils.cleanDirectory(new File("res/text/test/out"));
+		alert.setTitle("Result");
+		alert.setHeaderText("Lemmatization complete");
+		alert.setContentText("Done!");
+
+		alert.showAndWait();
+		
+		// otvara fajlove s rezultatima u sistemskom file browseru
+		try {
+			if (Desktop.isDesktopSupported()) {
+			    Desktop.getDesktop().open(new File(chosenFolder.toString() + "/out"));
+			}
+		} catch (IOException e) {
+			System.out.println("Unable to open file browser: " + e.getMessage());
+		}
 	}
 }
 
