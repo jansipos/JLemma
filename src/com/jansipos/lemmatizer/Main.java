48754e04641b664a5bbde20a701d8321b9aac1a0
Jan Sipos <jansipos@yahoo.com>
2015-06-14 16:17:18 +0200

    Progress bar

--- src/com/jansipos/lemmatizer/Main.java
+++ src/com/jansipos/lemmatizer/Main.java
@@ -6,11 +6,15 @@ import java.io.IOException;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -19,8 +23,15 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	private static String initialDir = "res/text/test";
+
+	private static Stage window;
+	private static VBox layout;
 	private static Button button;
+	
+	private static ProgressBar progressBar; 
+	
 	private static File chosenFolder;
+	private static List<File> chosenFiles;
 
 	Lemmatizer l = new Lemmatizer();
 
@@ -33,6 +44,15 @@ public class Main extends Application {
 
 	public void start(Stage primaryStage) throws Exception {
 
+		window = primaryStage;
+		
+		LemmaService service = new LemmaService();
+		service.setOnSucceeded(e -> {
+			displayDialogAndFiles();
+			service.reset();
+			progressBar.setVisible(false);
+		});
+		
 		button = new Button("Browse");
 		button.setOnAction(e -> {
 
@@ -43,20 +63,21 @@ public class Main extends Application {
 			FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("txt", "*.txt");
 			fc.getExtensionFilters().add(extFilter);
 
-			List<File> chosenFiles = fc.showOpenMultipleDialog(primaryStage);
-
-			if (chosenFiles != null && !chosenFiles.isEmpty()) {
-
-				chosenFolder = chosenFiles.get(0).getParentFile();
-
-				chosenFiles.forEach(f -> l.lemmatize(f));
-
-				displayDialogAndFiles();
-			}
+			chosenFiles = fc.showOpenMultipleDialog(window);
+			
+			service.start();
 		});
 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		layout = new VBox();
+		layout.setSpacing(10);
+		layout.setAlignment(Pos.CENTER);
+		
+		progressBar = new ProgressBar(0);
+		progressBar.setMinWidth(WIDTH - 100);
+		progressBar.setVisible(false);
+		progressBar.progressProperty().bind(service.progressProperty());
+		
+		layout.getChildren().addAll(button, progressBar);
 
 		Scene scene = new Scene(layout, WIDTH, HEIGHT);
 
@@ -82,6 +103,32 @@ public class Main extends Application {
 			}
 		} catch (IOException e) {
 			System.out.println("Unable to open file browser: " + e.getMessage());
+		}
+	}
+	
+	private class LemmaService extends Service<Void> {
+
+		@Override
+		protected Task<Void> createTask() {
+			
+			return new Task<Void>() {
+				
+				protected Void call() throws Exception {
+					
+					if (chosenFiles != null && !chosenFiles.isEmpty()) {
+						
+						chosenFolder = chosenFiles.get(0).getParentFile();
+						progressBar.setVisible(true);
+
+						for (int i = 0, len = chosenFiles.size(); i < len; i++) {
+							l.lemmatize(chosenFiles.get(i));
+							updateProgress(i, len);
+						}
+
+					}
+					return null;
+				}
+			};
 		}
 	}
 }
